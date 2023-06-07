fd6bc33a702164224f9ab2e624d6431fb7d8a196
paramburu22 <95660840+paramburu22@users.noreply.github.com>
2023-06-07 18:04:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tp3_hci/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.tp3_hci.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
