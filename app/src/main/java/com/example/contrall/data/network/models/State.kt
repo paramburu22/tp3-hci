93ade5f33806d8f3405b7dd6519627e01bfb2442
paramburu22 <95660840+paramburu22@users.noreply.github.com>
2023-06-19 16:56:09 -0300

    updated state

--- app/src/main/java/com/example/contrall/data/network/models/State.kt
+++ app/src/main/java/com/example/contrall/data/network/models/State.kt
@@ -3,22 +3,23 @@ package com.example.contrall.data.network.models
 import com.google.gson.annotations.SerializedName
 
 data class State (
-
-    @SerializedName("freezerTemperature" ) var freezerTemperature : Int?    = null,
+    //lamp
+    @SerializedName("status"             ) var status             : String? = null,
+    @SerializedName("color"              ) var color              : String? = null,
+    @SerializedName("brightness"         ) var brightness         : Int?    = null,
+    //ac
     @SerializedName("temperature"        ) var temperature        : Int?    = null,
+    @SerializedName("mode"               ) var mode               : String? = null,
     @SerializedName("verticalSwing"      ) var verticalSwing      : String? = null,
     @SerializedName("horizontalSwing"    ) var horizontalSwing    : String? = null,
     @SerializedName("fanSpeed"           ) var fanSpeed           : String? = null,
-    @SerializedName("status"             ) var status             : String? = null,
+    //speaker
+    @SerializedName("volume" ) var volume : Int?    = null,
+    @SerializedName("genre") var genre :String? = null,
+    //door
     @SerializedName("lock"               ) var lock               : String? = null,
-    @SerializedName("mode"               ) var mode               : String? = null,
-    @SerializedName("batteryLevel"       ) var batteryLevel       : Int?    = null,
-    @SerializedName("location"           ) var location           : Location? = null,
-    @SerializedName("color"              ) var color              : String? = null,
-    @SerializedName("brightness"         ) var brightness         : Int?    = null,
-)
-
-data class Location(
-    @SerializedName("id"                 ) var id                 : String? = null,
-    @SerializedName("name"               ) var name               : String? = null
+    //oven
+    @SerializedName("heat"       ) var heat       : String?    = null,
+    @SerializedName("grill"       ) var grill       : String?    = null,
+    @SerializedName("convection"       ) var convection       : String?    = null,
 )
